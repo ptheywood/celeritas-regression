3101ae91cf57fe20f1a390f310736d887b39f612
Peter Heywood <peethwd@gmail.com>
2025-02-18 17:25:43 +0000

    Bede configuraiton: enable nvidia smi pmon + reduce threads for gpu+g4

--- run-problems.py
+++ run-problems.py
@@ -149,17 +149,6 @@ class Wildstyle(System):
     gpu_per_job = 1
     cpu_per_job = 32
 
-class Bede(System):
-    #  User must be a member of the bdshe19 project on N8CIR Bede & have activated the spack env
-    build_dirs = {
-        'orange': Path("/nobackup/projects/bdshe19/aarch64/celeritas-project/celeritas/build-reldeb-novg"),
-        'vecgeom': Path("/nobackup/projects/bdshe19/aarch64/celeritas-project/celeritas/build-reldeb"),
-    }
-    name = "bede"
-    num_jobs = 1 # 1 GH200 480GB per gh partition node
-    gpu_per_job = 1
-    cpu_per_job = 72 # 72 CPU cores per GH200
-
 class Local(System):
     build_dirs = {
         "orange": Path("/Users/seth/Code/celeritas/build-reldeb"),
@@ -300,6 +289,47 @@ class Perlmutter(Frontier):
 
     def filter_problems(self, inputs):
         return inputs
+
+class Bede(System):
+    #  User must be a member of the bdshe19 project on N8CIR Bede & have activated the spack env
+    build_dirs = {
+        'orange': Path("/nobackup/projects/bdshe19/aarch64/celeritas-project/celeritas/build-reldeb-novg"),
+        'vecgeom': Path("/nobackup/projects/bdshe19/aarch64/celeritas-project/celeritas/build-reldeb"),
+    }
+    name = "bede"
+    num_jobs = 1 # 1 GH200 480GB per gh partition node
+    gpu_per_job = 1
+    cpu_per_job = 72 # 72 CPU cores per GH200
+    cpu_per_job_g4 = int(cpu_per_job // 2) # celer-g4 encounters cuda out of memory errors with high cpu per gpu count
+    power_sample_interval = 1.0  # seconds
+    
+    def create_gpu_power_monitor_subprocess(self, inp):
+        """
+        Create a subprocess that monitors GPU power usage using nvidia-smi
+
+        Must be using ampere+ GPUs, each sample measures the average power draw over 1s
+
+        Returns:
+            subprocess: the power monitor subprocess
+        """
+        cmd = "nvidia-smi"
+        args = ['dmon', '-i', str(inp['_instance']), '--select', 'pu', '--options', 'DT']
+        return asyncio.create_subprocess_exec(cmd, *args, stdout=subprocess.PIPE)
+    
+    def get_runtime_environ(self, inp):
+        # Get the environment for the generic System
+        env = super().get_runtime_environ(inp)
+
+        # celer-g4 encounters cuda out of mem errors on GH200 with 72 CPU threads but a single 96GiB GPU.
+        # use a different cpu count in this case.
+        if inp['_exe'] == "celer-g4":
+            env['OMP_NUM_THREADS'] = str(1)
+            env['G4FORCE_RUN_MANAGER_TYPE'] = "MT"
+            env['G4FORCENUMBEROFTHREADS'] = str(self.cpu_per_job_g4)
+        else:
+            assert inp['_exe'] == "celer-sim"
+        # return the updated environment
+        return env
 
 regression_dir = Path(__file__).parent
 input_dir = regression_dir / "input"
