b61c1a2646855b782100d1bb133fe69109b3db89
Peter Heywood <peethwd@gmail.com>
2025-02-20 13:30:26 +0000

    switch to testem3 as interactive test case

--- run-problems.py
+++ run-problems.py
@@ -577,11 +577,11 @@ use_vecgeom = {"_geometry": "vecgeom"}
 
 # List of list of setting dictionaries
 problems = [
-    [testem15, no_msc],
-    [testem15, no_msc, use_field],
+    # [testem15, no_msc],
+    # [testem15, no_msc, use_field],
     # [testem15, use_field],
     # [testem15, use_field, use_vecgeom],
-    # [testem3, no_msc],
+    [testem3, no_msc],
     # [testem3, no_msc, use_vecgeom],
     # [testem3, no_msc, use_field],
     # [testem3],
@@ -603,9 +603,9 @@ problems = [
 
 # Run again with sync on for detailed GPU timing
 sync_problems = [
-    [testem15, no_msc, use_field],
+    # [testem15, no_msc, use_field],
     # [testem15, no_msc, use_field, use_vecgeom],
-    # [testem3, use_field],
+    [testem3, use_field],
     # [testem3, use_field, use_vecgeom],
     # [testem3_composite, use_field],
     # [testem3_composite, use_field, use_vecgeom],
