bfd9b81bf3c9d17e39019345fc5eeba082ff35dd
Peter Heywood <peethwd@gmail.com>
2025-12-20 20:12:17 +0000

    fixup for non-jade machines x3

--- run-problems.py
+++ run-problems.py
@@ -378,7 +378,7 @@ class BedeFP32(System):
         return [i for i in inputs if i['_geometry'] == "orange" and i["_exe"] == "celer-sim"]
 
 class JadeARC(System):
-    _CELER_ROOT = Path(environ['DATA']) / 'shareing-r1' / 'celeritas' if 'DATA' in environ else ""
+    _CELER_ROOT = Path(environ['DATA']) / 'shareing-r1' / 'celeritas' if 'DATA' in environ else Path()
     build_dirs = {
         "orange": _CELER_ROOT / 'build-ndebug-novg'
     }
